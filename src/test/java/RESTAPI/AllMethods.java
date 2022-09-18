a28c19a68beb1f4506e2f582f48bf5d7d6844e65
Aruna <aruna.rudra76@gmail.com>
2022-09-18 13:35:11 +0530

    added comments

--- src/test/java/RESTAPI/AllMethods.java
+++ src/test/java/RESTAPI/AllMethods.java
@@ -14,6 +14,8 @@ public class AllMethods {
 	public Response response;
 	String baseuri = "https://reqres.in/api/users";
 	
+	//To generate random string for email field
+	
 	public static String randomString() {
 		String generatedString=RandomStringUtils.randomAlphabetic(5);
 		return(generatedString);
@@ -21,6 +23,7 @@ public class AllMethods {
 
 	}
 	
+	//GET Method to list all users
 	public Response GetAllUsers() {
 		
 		RestAssured.baseURI=baseuri;
@@ -29,6 +32,7 @@ public class AllMethods {
 		return response;
 	}
 	
+	//POST method to create a new user
 	public Response CreateUser() {
 		String uri= "https://gorest.co.in/public/v2/users";
 		JSONObject jobj= new JSONObject();
@@ -50,6 +54,8 @@ public class AllMethods {
 		
 	}
 	
+	//PUT Method to update the details of a specific user
+	
 	public Response PutMethod(int uid, String usrname, String gend) {
 		String uri= "https://gorest.co.in/public/v2/users";
 		JSONObject jobj= new JSONObject();
@@ -64,6 +70,7 @@ public class AllMethods {
 		
 	}
 	
+	//DELETE Method to delete the added user
 	public Response deleteMethod(int uid) {
 		String uri= "https://gorest.co.in/public/v2/users";
 		String token = "dba5075ef2bfe3035c8e6b448886449bd9533cf11638170a3f1b78d785f0e547";
@@ -73,6 +80,7 @@ public class AllMethods {
 		return response;
 	}
 	
+	//GET Method to check whether the user is present or not
 	public Response getSingleEmpMethod(int uid) {
 		String uri= "https://gorest.co.in/public/v2/users";
 		request=RestAssured.given();
